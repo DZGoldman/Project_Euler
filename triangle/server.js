a3dc9c1497c440eb80a55fa6a44819d5fd119d63
Daniel Goldman <dzgoldman@wesleyan.edu>
2016-01-31 18:18:02 -0500

    ready to submit triangle problem

--- triangle/server.js
+++ triangle/server.js
@@ -1,24 +1,30 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-//access the page
+//access the data by scraping it directly:
 request('http://www.yodlecareers.com/puzzles/triangle.txt', function (error, response, html) {
     var singleArray =html.split(' ')
     var twoDArray = [];
-
-//sort numbers into a 2D array
+//sort numbers into a 2D array (row 1 has 1 number, 2 has two numbers, etc):
+//create 100 rows
     for (var i = 1; i < 101; i++) {
-      var array = []
+      var row = []
+      //give each for appropriate length
       for (var j = 0; j < i; j++) {
+        //remove unwanted characters (/n and such)
         var num = +singleArray.shift()
-        array.push(num)
+        row.push(num)
       }
-      twoDArray.push(array)
+      //add row to 2D array
+      twoDArray.push(row)
     }
 
-//now,actually solve it
+//Now we have our data, time to actually solve the problem. The strategy will be to elimate one row at a time starting with the bottom row, as follows: for each number in the second to last row, look at the two numbers it could lead to in a path. Add to its value the greater of those two numbers. Once this is done for each number in the second to last row, the last row can be ignored, and the process then repeated for the 3rd to last row, etc. In the end, the single number at the top of the pyramid will be the answer. Here we go:
+
+// Starting with the 2d to last row, and working our way down:
 for (var i = 98; i >=0; i--) {
   twoDArray[i].forEach(function (number, index) {
+    //add the max of the two numbers it connects to in the row beneath it:
     var max= Math.max(twoDArray[i+1][index],twoDArray[i+1][index+1]  )
     twoDArray[i][index]=number+max
   })
